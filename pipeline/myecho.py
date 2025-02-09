577b6bcda1d8c9b2d2e0ff32d8f0a8a4f7467e23
Cullen Jon Navarre Roth <croth@pn2405732.lanl.gov>
2025-02-08 20:00:26 -0700

    attempt relavtive path correction.

--- pipeline/myecho.py
+++ pipeline/myecho.py
@@ -2,7 +2,7 @@
 ## Import sys 
 import sys
 ## Bring in write to file 
-from pipeline.defaults.tools.pysamtools import writetofile
+from SLURPY.pipeline.defaults.tools.pysamtools import writetofile
 
 ## Set the output message and output file 
 outmessage, outfile = ' '.join(sys.argv[1:-1]), sys.argv[-1]
