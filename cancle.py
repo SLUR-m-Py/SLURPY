a793335e9387baa2f57a3f74bf3bafe9b14ce175
Cullen Jon Navarre Roth <croth@pn2405732.lanl.gov>
2025-02-08 18:32:29 -0700

    Corrected typo in cancle.py.

--- cancle.py
+++ cancle.py
@@ -6,7 +6,7 @@ squeue -u croth | grep 'croth' | awk '{print $1}' | xargs -n 1 scancel
 ## Bring in mods 
 import sys, os
 ## Set user nmae
-user_name = sys.arg[1]
+user_name = sys.argv[1]
 
 ## Cancel command
 can_comand = 'squeue -u %s | grep \'%s\' | awk \'\{print $1\}\' | xargs -n 1 scancel'%user_name
