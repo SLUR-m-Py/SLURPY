5c688a58d04b9f4257b95f8bd9b638e695b9d43c
Cullen Jon Navarre Roth <croth@pn2405732.lanl.gov>
2025-03-12 11:28:20 -0600

    Replaced error and stdout log in inter2count.py

--- inter2count.py
+++ inter2count.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 #SBATCH --job-name=inter.count          ## Name of job
-#SBATCH -o inter.count.out              ## Name of output for std err and log
+#SBATCH --output=%x.%j.out              ## Name stdout
+#SBATCH --error=%x.%j.err               ## Name stderr
 #SBATCH --nodes=1                       ## Number of nodes needed for the job
 #SBATCH --ntasks-per-node=1             ## Number of tasks to be launched per Node
 #SBATCH --cpus-per-task=12              ## Number of tasks to be launched
