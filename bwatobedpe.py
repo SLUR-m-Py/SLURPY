84b290ea212dba984b50f37cfd03f1e8068da725
Cullen Jon Navarre Roth <croth@pn2405732.lanl.gov>
2025-03-26 21:16:21 -0600

    added minimum seed of 10 to bwa.

--- bwatobedpe.py
+++ bwatobedpe.py
@@ -161,7 +161,7 @@ if __name__ == "__main__":
 
         ## format the command 
         bwa_coms = [f'refpath={ref_path}\n',
-                    f'bwa mem -v 1 -t {thread_count-1} {options} $refpath {r1} {r2} | {slurpydir}/tobedpe.py $refpath {library} {outfile} {line_count}\n',
+                    f'bwa mem -v 1 -k 10 -t {thread_count-1} {options} $refpath {r1} {r2} | {slurpydir}/tobedpe.py $refpath {library} {outfile} {line_count}\n',
                     f'{slurpydir}/myecho.py Finished bwa alignment of split {i} {bwa_check}\n## EOF']
 
         ## If the report exists and has alredy been run, just skip
